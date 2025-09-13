50ccc2d7735803dcb273559ae07032eb593ea785
August Rouvari <august.rouvari@helsinki.fi>
2025-09-13 14:38:36 +0300

    Add database

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import db
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    albums = db.query("SELECT title, artist, year, genre FROM albums")
+    return render_template("index.html", albums=albums)
 
 @app.route("/add")
 def add():
@@ -14,5 +16,7 @@ def add():
 def result():
     title = request.form["title"]
     artist = request.form["artist"]
+    year = request.form["year"]
     genre = request.form["genre"]
-    return render_template("result.html", title=title, artist=artist, genre=genre)
+    db.query("INSERT INTO albums (title, artist, year, genre) VALUES (?, ?, ?, ?)", (title, artist, year, genre))
+    return render_template("result.html", title=title, artist=artist, year=year, genre=genre)
