17bc2c120f4bab9c091f093b64e80eedfd3a92af
August Rouvari <august.rouvari@helsinki.fi>
2025-09-13 15:20:46 +0300

    Add registration

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect
+from werkzeug.security import generate_password_hash
+import sqlite3
 import db
 
 app = Flask(__name__)
@@ -18,5 +20,25 @@ def result():
     artist = request.form["artist"]
     year = request.form["year"]
     genre = request.form["genre"]
-    db.query("INSERT INTO albums (title, artist, year, genre) VALUES (?, ?, ?, ?)", (title, artist, year, genre))
+    db.execute("INSERT INTO albums (title, artist, year, genre) VALUES (?, ?, ?, ?)", (title, artist, year, genre))
     return render_template("result.html", title=title, artist=artist, year=year, genre=genre)
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "error: Passwords don't match"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        db.execute("INSERT INTO users (username, password_hash) VALUES (?, ?)", (username, password_hash))
+    except sqlite3.IntegrityError:
+        return "error: Username taken"
+
+    return redirect("/")
