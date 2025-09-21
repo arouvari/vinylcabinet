3047be35f379a8e2f01d836492f7d55b9a66b597
August Rouvari <august.rouvari@helsinki.fi>
2025-09-21 10:35:09 +0300

    Add schema and gitignore

---
+++ schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS albums (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    artist TEXT NOT NULL,
+    year TEXT,
+    genre TEXT,
+    user_id INTEGER NOT NULL,
+    image_url TEXT,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS favorites (
+    user_id INTEGER NOT NULL,
+    album_id INTEGER NOT NULL,
+    PRIMARY KEY (user_id, album_id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (album_id) REFERENCES albums (id)
+);
